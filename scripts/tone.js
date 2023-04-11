4115a9cffaee2e2bb02a174b08797379a8fe828c
rvdegroen <rvdegroen@gmail.com>
2023-04-12 00:06:37 +0200

    removed duplicated synth

--- scripts/tone.js
+++ scripts/tone.js
@@ -1,8 +1,5 @@
 import * as Tone from 'tone';
 
-//create a synth and connect it to the main output (your speakers)
-const synth = new Tone.Synth().toDestination();
-
 // finds first button then adds event listener to it
 document.querySelector('button')?.addEventListener('click', async () => {
     await Tone.start();
