f1ce902e0ed7c66de0da0415bb79def2ab83cdff
Laurens256 <duinlaurens2@gmail.com>
2023-04-12 11:41:09 +0200

    dumbass loop

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,37 @@
-import { playSound } from './tone';
+import * as Tone from 'tone';
 
-const init = () => {
+const sampler = new Tone.Sampler({
+    urls: {
+        A1: 'sample_3.wav'
+    },
+    baseUrl: '/assets/audio/test_samples/',
+}).toDestination();
 
+const steps = 8;
+let currentStep = 0;
+
+const playAudio = async () => {
+	await Tone.start();
+	Tone.Transport.stop();
+	Tone.Transport.cancel();
+
+	Tone.Transport.scheduleRepeat((time) => {
+		currentStep = (currentStep % steps) + 1;
+		sampler.triggerAttackRelease('A1', '8n', time);
+	});
+
+	Tone.Transport.start();
 };
 
-const btn = document.querySelector('button');
-btn.addEventListener('click', playSound);
+const pauseAudio = () => {
+	Tone.Transport.stop();
+	currentStep = 0;
+};
+
+
+const playButton = document.querySelector('button.playbtn');
+const pauseButton = document.querySelector('button.pausebtn');
+
+playButton.addEventListener('click', playAudio);
+pauseButton.addEventListener('click', pauseAudio);
+
